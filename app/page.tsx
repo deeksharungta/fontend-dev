d863651f0d3e138c432e5b923762244c145bfc95
Deeksha Rungta <deeksharu1902@gmail.com>
2024-02-19 05:10:57 +0530

    feat: implemented responsive slider

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Shades from "@/components/Shades/Shades";
 import Models from "@/components/AIModels/Models";
 import AskAIButton from "@/components/AskAI/AskAIButton";
 import SelectItemMenu from "@/components/SelectItemMenu/SelectItemMenu";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { ColorContext } from "@/store/color-context";
 
 export default function Home() {
@@ -34,6 +34,50 @@ export default function Home() {
     document.body.style.setProperty("--clock-color", clockColor);
   }, [color, shades]);
 
+  const blockRef = useRef<HTMLDivElement>(null);
+  const sliderRef = useRef<HTMLDivElement>(null);
+  // const blocksRef = useRef<HTMLDivElement>(null);
+  // const sliderVerRef = useRef<HTMLDivElement>(null);
+
+  const dragMouseDown = (e: any) => {
+    if (!blockRef.current || !sliderRef.current) return;
+
+    let dragX = e.clientX;
+    document.onmousemove = (event) => {
+      blockRef.current!.style.width =
+        blockRef.current!.offsetWidth - event.clientX + dragX + "px";
+      dragX = event.clientX;
+    };
+
+    document.onmouseup = () => {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    };
+  };
+
+  // const dragMouseDowns = (e: any) => {
+  //   if (!blocksRef.current || !sliderVerRef.current) return;
+
+  //   let dragY = e.clientY;
+  //   document.onmousemove = (event) => {
+  //     blocksRef.current!.style.height =
+  //       blocksRef.current!.offsetHeight + event.clientY - dragY + "px";
+  //     dragY = event.clientY;
+  //   };
+
+  //   document.onmouseup = () => {
+  //     document.onmousemove = null;
+  //     document.onmouseup = null;
+  //   };
+  // };
+
+  useEffect(() => {
+    return () => {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    };
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.top}>
@@ -43,9 +87,25 @@ export default function Home() {
       <div className={styles.content}>
         <div className={styles.first}>
           <Tasks />
-          <Models />
+          <div
+            className={styles.slider}
+            ref={sliderRef}
+            onMouseDown={dragMouseDown}
+          >
+            <div className={styles["slider-thumb"]} />
+          </div>
+          <div ref={blockRef} className={styles.model}>
+            <Models />
+          </div>
           <DragDrop />
         </div>
+        {/* <div
+          className={styles["slider-horizontal"]}
+          ref={sliderVerRef}
+          onMouseDown={dragMouseDowns}
+        >
+          <div className={styles["slider-thumb-horizontal"]} />
+        </div> */}
         <div className={styles.second}>
           <BeforeAfter />
           <Loader />
