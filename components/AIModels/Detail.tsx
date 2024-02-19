41fd21a83dcdfc1d8bc3365e7ff7a326fda02a1d
Deeksha Rungta <deeksharu1902@gmail.com>
2024-02-19 19:19:52 +0530

    changed checkbox animation

--- components/AIModels/Detail.tsx
+++ components/AIModels/Detail.tsx
@@ -5,9 +5,8 @@ const Detail = () => {
     <div className={styles.container}>
       <p>
         ChatGPT is a chatbot developed by OpenAI and launched on November 30,
-        2022. Based on a large language model, it enables users to refine and
-        steer a conversation towards a desired length, format, style, level of
-        detail, and language.
+        2022. It enables users to refine and steer a conversation towards a
+        desired length, format, style, level of detail, and language.
       </p>
     </div>
   );
